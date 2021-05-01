85be57ce91b8ee947ecb5cceef3a79619773f915
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-05-01 20:30:57 -0300

    Realizando tratamento de Erros - 01/05/2021

--- src/main/java/com/javabugs/logmanager/controller/advice/ApplicationControllerAdvice.java
+++ src/main/java/com/javabugs/logmanager/controller/advice/ApplicationControllerAdvice.java
@@ -1,19 +1,52 @@
 package com.javabugs.logmanager.controller.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public String handleConstraintViolationException(ConstraintViolationException ex) {
+//        Map<String, String> errors = new HashMap<>();
+//        ex.get getBindingResult().getAllErrors().forEach((error) -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//        return errors;
+        return ex.getMessage();
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public String handleException(Exception e) {
-        return "Internal Server Error: " + e.getMessage();
+        return "Internal Server Error: " + e;
     }
 
 }
